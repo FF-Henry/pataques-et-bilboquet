567d5bf0062806327d86a930cf8899696347eed5
Admin <henry.felix-faure@student.junia.com>
2023-11-30 13:47:41 +0100

    premiere necessite

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include "raylib.h"
 
-constexpr auto SCREEN_WIDTH  = 800;
-constexpr auto SCREEN_HEIGHT = 450;
+constexpr auto SCREEN_WIDTH  = 1600;
+constexpr auto SCREEN_HEIGHT = 900;
 
 int main()
 {
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Window title");
-    SetTargetFPS(60);
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Pataquès et bilboquet");
+    SetTargetFPS(30);
 
     Texture2D texture = LoadTexture(ASSETS_PATH"test.png");
 
@@ -14,13 +14,13 @@ int main()
     {
         BeginDrawing();
 
-        ClearBackground(RAYWHITE);
+        ClearBackground(WHITE);
 
         const int texture_x = SCREEN_WIDTH / 2 - texture.width / 2;
         const int texture_y = SCREEN_HEIGHT / 2 - texture.height / 2;
         DrawTexture(texture, texture_x, texture_y, WHITE);
 
-        const char* text = "OMG! IT WORKS!";
+        const char* text = "EDGAR";
         const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
         DrawText(text, SCREEN_WIDTH / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
 
